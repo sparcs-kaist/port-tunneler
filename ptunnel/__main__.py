3c58ea9c5a038f6c308645ba375caf71f0412c68
DoyunShin <doyun.shin@gmail.com>
2024-02-15 10:48:02 +0900

    Version 1.1.0

--- ptunnel/__main__.py
+++ ptunnel/__main__.py
@@ -5,7 +5,7 @@ import ptunnel
 import ptunnel.client
 import ptunnel.server
 
-__version__ = "1.1.0-pre11"
+__version__ = "1.1.0"
 ptunnel.__version__ = __version__
 
 @click.version_option(prog_name="ptunnel", version=__version__)
